4a7c5608944451f7c5b38bde2f89ec9536f6b676
hugoplouzeau <130675257+hugoplouzeau@users.noreply.github.com>
2025-06-05 16:32:08 +0200

    project setting changement

--- Assets/Scripts/InkEditor/BasicInkExampleEditor.cs
+++ Assets/Scripts/InkEditor/BasicInkExampleEditor.cs
@@ -1,26 +1,26 @@
-﻿using Ink.Runtime;
-using Ink.UnityIntegration;
-using UnityEditor;
-using UnityEngine;
+﻿//using Ink.Runtime;
+//using Ink.UnityIntegration;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomEditor(typeof(BasicInkExample))]
-[InitializeOnLoad]
-public class BasicInkExampleEditor : Editor {
-    static bool storyExpanded;
-    static BasicInkExampleEditor () {
-        BasicInkExample.OnCreateStory += OnCreateStory;
-    }
+//[CustomEditor(typeof(BasicInkExample))]
+//[InitializeOnLoad]
+//public class BasicInkExampleEditor : Editor {
+//    static bool storyExpanded;
+//    static BasicInkExampleEditor () {
+//        BasicInkExample.OnCreateStory += OnCreateStory;
+//    }
 
-    static void OnCreateStory (Story story) {
-        // If you'd like NOT to automatically show the window and attach (your teammates may appreciate it!) then replace "true" with "false" here. 
-        InkPlayerWindow window = InkPlayerWindow.GetWindow(false);
-        if(window != null) InkPlayerWindow.Attach(story);
-    }
-	public override void OnInspectorGUI () {
-		Repaint();
-		base.OnInspectorGUI ();
-		var realTarget = target as BasicInkExample;
-		var story = realTarget.story;
-		InkPlayerWindow.DrawStoryPropertyField(story, ref storyExpanded, new GUIContent("Story"));
-	}
-}
+//    static void OnCreateStory (Story story) {
+//        // If you'd like NOT to automatically show the window and attach (your teammates may appreciate it!) then replace "true" with "false" here. 
+//        InkPlayerWindow window = InkPlayerWindow.GetWindow(false);
+//        if(window != null) InkPlayerWindow.Attach(story);
+//    }
+//	public override void OnInspectorGUI () {
+//		Repaint();
+//		base.OnInspectorGUI ();
+//		var realTarget = target as BasicInkExample;
+//		var story = realTarget.story;
+//		InkPlayerWindow.DrawStoryPropertyField(story, ref storyExpanded, new GUIContent("Story"));
+//	}
+//}
